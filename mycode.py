36512b5651cf0b95368477618c974e6678658767
Turtilla <98593377+Turtilla@users.noreply.github.com>
2022-03-01 13:51:34 +0100

    Part 3

--- mycode.py
+++ mycode.py
@@ -2,10 +2,13 @@ import gzip
 import random
 from nltk.tokenize import WordPunctTokenizer
 from nltk.corpus import stopwords
+from nltk.util import ngrams
+import numpy as np
+import pandas as pd
 import nltk
 nltk.download('stopwords')
 nltk.download('averaged_perceptron_tagger')
-from nltk.util import ngrams
+
 
 def sample_lines(zipped_file, lines=100000):
     unzipped_file = gzip.open(zipped_file, "rb")
@@ -64,10 +67,10 @@ def create_samples(processed_sentences, samples=50000):
 
     all_samples = []
     for pair1, pair2, pair3, pair4, pair5 in samples:
-        word1 = get_ending(pair1[0])
-        word2 = get_ending(pair2[0])
-        word4 = get_ending(pair4[0])
-        word5 = get_ending(pair5[0])
+        word1 = get_ending(pair1[0]) + '_1'
+        word2 = get_ending(pair2[0]) + '_2'
+        word4 = get_ending(pair4[0]) + '_4'
+        word5 = get_ending(pair5[0]) + '_5'
         POS = pair3[1]
 
         if "V" in POS:
@@ -84,17 +87,48 @@ def get_ending(word):
     ending = word[-2:]
     return ending
 
+def create_df(all_samples):
+    possible_columns = []
+    possible_columns_set = set()
+    for sample in all_samples:
+        endings = sample[0]
+        for ending in endings:
+            if ending not in possible_columns_set:
+                possible_columns.append(ending)
+                possible_columns_set.add(ending)
+    possible_columns.append('verb')
+    
+    sample_vectors = []
+    for sample in all_samples:
+        endings = sample[0]
+        ifverb = sample[1]
+        vector = []
+        for label in possible_columns:
+            if label != 'verb':
+                if label in endings:
+                    vector.append(1)
+                else:  # if label not in endings
+                    vector.append(0)
+        vector.append(ifverb)
+        sample_vectors.append(vector)
+
+    vector_array = np.array(sample_vectors)
+    vector_df = pd.DataFrame(vector_array, columns=possible_columns)
+
+    return vector_df
+
 
 
 if __name__ == "__main__":
-    sampled_lines = sample_lines(r"C:\Users\turti\OneDrive\Dokumenty\GitHub\machine_learning\UN-english.txt.gz", lines=100000)
+    sampled_lines = sample_lines(r"C:\Users\turti\OneDrive\Dokumenty\GitHub\machine_learning\UN-english.txt.gz", lines=10000)
     print(len(sampled_lines))
-    print(sampled_lines[40000:40010])
+    print(sampled_lines[4000:4010])
 
     processed_sentences = process_sentences(sampled_lines)
-    print(processed_sentences[40000:40010])
+    print(processed_sentences[4000:4010])
 
-    all_samples = create_samples(processed_sentences, samples=50000)
-    print(all_samples[25000:25010])
+    all_samples = create_samples(processed_sentences, samples=5000)
+    print(all_samples[2500:2510])
 
-    print(get_ending('quickly'))
+    fulldf = create_df(all_samples)
+    print(fulldf[2500:2510])
